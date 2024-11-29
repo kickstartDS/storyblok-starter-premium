d8430800d66c8d90d23cf6e3b9f546740bee1d1b
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2024-11-29 12:19:19 +0100

    fix: accept className prop in InlineIcon

--- scripts/svgsToReactComponent.js
+++ scripts/svgsToReactComponent.js
@@ -2,19 +2,30 @@ const { parse } = require("svg-parser");
 const toJsx = require("@mapbox/hast-util-to-jsx");
 
 module.exports = ({ dictionary }) => {
-  let result = "const icons: Record<string, JSX.Element> = {\n";
+  let result = `\
+import { FC, SVGAttributes } from "react";
+import classNames from "classnames";
+import type { IconProps } from "@kickstartds/base/lib/icon/typing";
+
+const icons: Record<string, FC<SVGAttributes<SVGElement>>> = {
+`;
 
   for (const icon of Object.values(dictionary.tokens.icons)) {
     const tree = parse(icon.value);
-    const svgProperties = (tree.children[0].properties ??= {});
-    svgProperties.class = (svgProperties.class ? " " : "") + "icon";
     const jsx = toJsx(tree);
-    result += `  "${icon.name}": ${jsx},\n`;
+    result += `  "${icon.name}": (props) => ${jsx.replace(
+      "<svg ",
+      "<svg {...props} "
+    )},\n`;
   }
-  result += "};";
+  result += "};\n";
 
   result += `
-export const InlineIcon = ({ icon }: { icon: string }) => icons[icon];
+export const InlineIcon: FC<IconProps & SVGAttributes<SVGElement>> = ({
+  icon,
+  className,
+  ...props
+}) => icons[icon]({className: classNames("icon", className), ...props});
 `;
   return result;
 };
