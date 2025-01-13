9d731a61b4e3588b52eaf879fa47d5026ef8b60f
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2025-01-13 14:41:50 +0100

    fix: add Clarity to allowed script sources

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const cspHeader = `
     default-src 'self';
     connect-src 'self' https://data.debugbear.com;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.storyblok.com https://cdn.debugbear.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.storyblok.com https://cdn.debugbear.com https://www.clarity.ms;
     style-src 'self' 'unsafe-inline';
     frame-src 'self' https://youtube.com https://www.youtube.com https://player.vimeo.com *.google.com;
     img-src 'self' blob: data: https://a.storyblok.com;
