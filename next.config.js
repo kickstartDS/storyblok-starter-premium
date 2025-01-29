2c029d749519cddfa2323a25e6a1c8ee404f431f
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2025-01-29 16:36:57 +0100

    fix: add more script location excemtions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const cspHeader = `
     default-src 'self';
-    connect-src 'self' https://data.debugbear.com https://*.clarity.ms;
+    connect-src 'self' https://data.debugbear.com https://*.clarity.ms https://app.lemcal.com;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.storyblok.com https://cdn.debugbear.com https://www.clarity.ms https://cdn.lemcal.com;
-    style-src 'self' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     frame-src 'self' https://youtube.com https://www.youtube.com https://player.vimeo.com *.google.com;
     img-src 'self' blob: data: https://a.storyblok.com;
     media-src 'self' blob: data: https://a.storyblok.com;
