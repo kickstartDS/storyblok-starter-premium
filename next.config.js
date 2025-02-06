d7db71f2a1d942921e45541f13106e12b77ed206
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2025-02-06 17:03:49 +0100

    fix: added yt nocookie

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const cspHeader = `
     connect-src 'self' https://data.debugbear.com https://*.clarity.ms https://app.lemcal.com;
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.storyblok.com https://cdn.debugbear.com https://www.clarity.ms https://cdn.lemcal.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    frame-src 'self' https://youtube.com https://www.youtube.com https://player.vimeo.com *.google.com;
+    frame-src 'self' https://youtube.com https://www.youtube.com https://player.vimeo.com https://www.youtube-nocookie.com/ *.google.com;
     img-src 'self' blob: data: https://a.storyblok.com https://assets.lemcal.com https://videos.lemcal.com https://app.lemcal.com;
     media-src 'self' blob: data: https://a.storyblok.com;
     font-src 'self' https://fonts.gstatic.com;
